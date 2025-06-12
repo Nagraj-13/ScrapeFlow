dbf08f7d19bfca49eedc334ac198ecb7fa61b018
OneBoatSolutions <nagrajy131200350@gmail.com>
2025-06-12 13:12:47 +0530

    feat: Implement NodeCard

---
+++ app/workflow/_components/nodes/params/StringParam.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { ParamProps } from '@/types/appNode'
+import React, { useId, useState } from 'react'
+
+
+function StringParam({param, value, updateNodeParamValue}: ParamProps) {
+    const [internalValue, setInternalValue] = useState(value)
+    const id = useId()
+  return (
+    <div className='space-y-1 p-1 w-full'>
+        <Label htmlFor={id} className='text-xs flex'>
+            {param.name} 
+            {param.required && <p className='text-red-400'>*</p>}
+        </Label>
+        <Input 
+            id={id} 
+            value={internalValue} 
+            className='text-xs'
+            placeholder='Enter value here'
+            onChange={(e) => setInternalValue(e.target.value)}
+            onBlur={(e) => updateNodeParamValue(e.target.value)}
+        />
+        {param.helperText && (
+            <p className='text-muted-foreground text-sm px-2'>
+                {param.helperText}
+            </p>
+        )}
+    </div>
+  )
+}
+
+export default StringParam
